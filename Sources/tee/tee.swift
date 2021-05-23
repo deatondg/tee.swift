58382e2b9157a018294b8d5eafc1711e1ec68e2b
deatondg <deaton.dg@gmail.com>
2021-05-23 01:51:55 -0400

    Fixed a small typo in the comments

--- Sources/tee/tee.swift
+++ Sources/tee/tee.swift
@@ -6,7 +6,7 @@ import Foundation
     we accept the type `Any`, but throw a precondition failure if the arguments are not of type `Pipe` or `FileHandle`.
  https://github.com/apple/swift-corelibs-foundation/blob/eec4b26deee34edb7664ddd9c1222492a399d122/Sources/Foundation/Process.swift
  When `input` sends an EOF (write of length 0), the `outputs` file handles are closed, so only output to handles you own.
- This function sets the `readabilityHandler` of inputs and the `writabilityHandler` of outputs,
+ This function sets the `readabilityHandler` of inputs and the `writeabilityHandler` of outputs,
     so you should not set these yourself after calling `tee`.
  The one exception to this guidance is that you can set the `readabilityHandler` of `input` to `nil` to stop `tee`ing.
  After doing so, the `writeabilityHandler`s of the `output`s will be set to `nil` automatically after all in-progress writes complete,
